d1ba39528a302d5b1ae0c2efff00d88ea0228576
Christopher Read <Christopherread22@outlook.com>
2022-05-10 20:16:29 -0400

    setting up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Navbar from './Components/Navbar/Index';
 
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Pizza</h1>
+      <Navbar />
     </div>
   );
 }
