aa7d44f511ed842d9a35463566521dac2355715c
Christopher Read <Christopherread22@outlook.com>
2022-05-26 21:41:04 -0400

    globalStyle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './components/globalStyles';
 
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Navbar />
     </Router>
   );
